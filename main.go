8b9c32febe6df91ca6df24ec473aed2151c5bc66
XyrelTenz <xdemocrito1@gmail.com>
2025-10-22 13:07:27 +0800

    Variadic Functions

--- main.go
+++ main.go
@@ -4,15 +4,33 @@ import (
 	"fmt"
 )
 
+func sum(nums ...int) {
+	fmt.Print(nums, " ")
+
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(" = ", total)
+}
+
+func Calculator(num1 int, num2 int) (calculate int) {
+	fmt.Print("First Number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Second Number: ")
+	fmt.Scanln(&num2)
+
+	calculate = num1 + num2
+	return calculate
+}
+
 func main() {
 
 	// miniProjects.BankSystem(&miniProjects.BankStruct{})
 	// DSA.Fibonnaci()
 	// Playground.Validation()
 
-	defer func() {
-		fmt.Println("Second")
-	}()
-	fmt.Println("First")
-
+	sum(1, 2, 3, 4, 5)
+	Calculator(2, 3)
 }
